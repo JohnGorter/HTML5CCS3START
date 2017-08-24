a9ea503b8a89f20d588ae73ea0175da0697b92b8
john gorter <john.gorter@gmail.com>
2017-08-24 15:31:39 +0200

    'init'

--- index.js
+++ index.js
@@ -2,21 +2,29 @@
 var selectedTodo = null;
 
 function saveTodo(){
-    var title = document.getElementById("title").value;
-    var omschrijving = document.getElementById("omschrijving").value;
-    var todolistul = document.getElementById("todolistul");
-
-    var li = document.createElement("li");
-    li.innerHTML = '' + title + ' <span>' + omschrijving + ' </span>';
-
-
-    todolistul.appendChild(li);
+    var titleinput = document.getElementById("title");
+    var omschrijvinginput = document.getElementById("omschrijving")
+    if (titleinput.validity.valid && omschrijvinginput.validity.valid) {
+        var title = titleinput.value;
+        var omschrijving = omschrijvinginput.value;
+        var todolistul = document.getElementById("todolistul");
+        var li = document.createElement("li");
+        li.innerHTML = '' + title + ' <span>' + omschrijving + ' </span>';
+        todolistul.appendChild(li);
+        document.querySelector("#noitems").hidden =!(todolistul.childElementCount == 0);
+        document.getElementById("error").innerHTML = '';
+        titleinput.value = '';
+        omschrijvinginput.value = '';
+    } else {
+        document.getElementById("error").innerHTML = 'Ongeldige invoer, probeer opnieuw!';
+    }
 }
 
 function removeTodo(){
     if (selectedTodo){
         var todolistul = document.getElementById("todolistul");
         todolistul.removeChild(selectedTodo);
+        document.querySelector("#noitems").hidden = !(todolistul.childElementCount == 0);
         selectedTodo = null;
         showDetails(selectedTodo);
     }
@@ -29,10 +37,27 @@ function selectTodo(){
 }
 
 function showDetails(li){
-    console.log('event', event.target);
     var title = document.getElementById("todoTitle");
     var desc = document.getElementById("todoDescription");
-    title.innerHTML = li ? li.innerText : "";
-    desc.innerHTML = li? li.getElementsByTagName("span")[0].innerText : "";
-    btnDone.style.display = li ? "block" :"none";
+    var details = document.querySelector("#tododetail")
+    if (li) {
+        title.innerHTML = li.innerText;
+        desc.innerHTML = li.getElementsByTagName("span")[0].innerText;
+        btnDone.style.display = "block";
+    }
+    if (!li || (li && document.querySelector("#tododetail").hidden)) {
+        if (li) {
+             document.querySelector("#tododetail").hidden = false;
+             document.querySelector("#tododetail").style.opacity = '1';
+        }
+        var player = details.animate([{opacity:li?'0':'1'}, {opacity:li?'1':'0'}], {duration:700,easing:'ease-in-out'});
+        player.onfinish = () => {
+            document.querySelector("#tododetail").hidden = !li;
+            document.querySelector("#tododetail").style.opacity = li ? '1' : '0';
+            title.innerHTML = li ? li.innerText : "";
+            desc.innerHTML = li? li.getElementsByTagName("span")[0].innerText : "";
+            btnDone.style.display = li ? "block" :"none";
+        };
+    }
+    
 }
